e3600ed8d5d4af04c0b5f4de4c9df814a37f2d17
Enzo POGGIO <poggio.enzo@gmail.com>
2019-02-10 01:22:29 +0100

    Add box add color rm dd

--- main.py
+++ main.py
@@ -8,54 +8,58 @@ from framework.gamemanager import *
 import curses
 import sys
 
-from collections import defaultdict as dd
+
+#from collections import defaultdict as dd
 
 import time
 
 def main(stdscr):
-    stdscr.clear()
+	stdscr.clear()
 
-    if curses.COLS < MAP_WIDTH - 1 or curses.LINES < MAP_HEIGHT - 1:
-        print("Terminal Too Small", file=sys.stderr)
-        sys.exit(-1)
+	if curses.COLS < MAP_WIDTH - 1 or curses.LINES < MAP_HEIGHT - 1:
+		print("Terminal Too Small", file=sys.stderr)
+		return (64)
+	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+	curses.curs_set(0)
+	win = curses.newwin(MAP_HEIGHT, MAP_WIDTH, WIN_Y, WIN_X)
+	win.keypad(True)
+	win.box()
+	doorA = Door(19, 40)
+	doorB = Door(55, 60)
 
-    curses.curs_set(0)
-    win = curses.newwin(MAP_HEIGHT, MAP_WIDTH, WIN_Y, WIN_X)
-    win.keypad(True)
+	roomA = Room(1, 1, 30, 40, [doorA])
+	roomB = Room(50, 60, 10, 10, [doorB])
 
-    doorA = Door(18, 39)
-    doorB = Door(55, 60)
+	board = Board([roomA, roomB], [(doorA, doorB)])
 
-    roomA = Room(0, 0, 30, 40, [doorA])
-    roomB = Room(50, 60, 10, 10, [doorB])
-
-    board = Board([roomA, roomB], [(doorA, doorB)])
-
-    Manager = GameManager(board)
-    Manager.loadItems('items.json')
-    Manager.loadMonsters('mobs.json')
-    Manager.placeItem(50)
-    Manager.placeMob(2)
+	Manager = GameManager(board)
+	Manager.loadItems('items.json')
+	Manager.loadMonsters('mobs.json')
+	Manager.placeItem(50)
+	Manager.placeMob(2)
    # Manager.printBoard()
-    while True:
-        win.erase()
+	while True:
+		win.erase()
 
-        for _, gameObject in board.all.items():
-            win.addstr(gameObject.y, gameObject.x, gameObject.sym)
+		for _, gameObject in board.all.items():
+			win.addstr(gameObject.y, gameObject.x, gameObject.sym)
 
-        for coord in Manager.placedItems:
-            item = Manager.placedItems[coord]
-            win.addstr(item.y, item.x, item.sym)
+		for coord in Manager.placedItems:
+			item = Manager.placedItems[coord]
+			win.addstr(item.y, item.x, item.sym, curses.color_pair(3))
 
-        for coord in Manager.placedMobs:
-            item = Manager.placedMobs[coord]
-            win.addstr(item.y, item.x, item.sym)
-        
-        win.addstr(Manager.player.y, Manager.player.x, "\u263b")
-        win.refresh()
-        key = win.getch()
-        Manager.update(key)
+		for coord in Manager.placedMobs:
+			item = Manager.placedMobs[coord]
+			win.addstr(item.y, item.x, item.sym, curses.color_pair(2))
+
+		win.addstr(Manager.player.y, Manager.player.x, '\u263A', curses.color_pair(1))
+		win.border('|', '|', '-', '-', '+', '+', '+', '+')
+		win.refresh()
+		key = win.getch()
+		Manager.update(key)
 
 
 if __name__ == "__main__":
-    wrapper(main)
+	wrapper(main)
